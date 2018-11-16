cd759ce4302985e252db5abc3e7e55ab7d138386
Nicolas <nicolas@Nicolass-MacBook-Pro.local>
2018-11-16 01:15:36 +0100

    add a bit of q learning. Q(s,{right,down}) don't converge for some reason..

--- Lab1/Problem3.py
+++ Lab1/Problem3.py
@@ -1,5 +1,5 @@
-import numpy as np;
-
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 # GAME PROPRETIES
@@ -14,16 +14,25 @@ DOWN = BOARD_WIDTH
 LEFT = -1
 
 ROBBER_ACTIONS = [STAY, UP, RIGHT, DOWN, LEFT]
-POLICE_ACTIONS = [UP,RIGHT,DOWN,LEFT]
+POLICE_ACTIONS = [UP, RIGHT, DOWN, LEFT]
 
-N_ROBBER_ACTIONS=len(ROBBER_ACTIONS);
-N_POLICE_ACTIONS=len(POLICE_ACTIONS);
+N_ROBBER_ACTIONS = len(ROBBER_ACTIONS)
+N_POLICE_ACTIONS = len(POLICE_ACTIONS)
 
-N_STATES=N_TILES**2;
+N_STATES = N_TILES**2
 
-#PARAMETERS
+INITAL_STATE=0;
 
-discount_factor=0.8;
+# PARAMETERS
+
+n_iterations=10**6;
+discount_factor = 0.8
+learning_rate=0.05;
+
+##PLOTTING PARAMS
+
+ivs_step=100;
+
 
 
 def rc2idx(rc):
@@ -72,23 +81,47 @@ def move(current_pos, action):
 
 def reward(state, action):
     '''computes reward for a given state and action'''
-    reward=0;
-    player_idx,police_idx=si2ai(state);
-    if player_idx==5:
-    	reward+=1;
-    if police_idx==player_idx:
-    	reward-=10;
+    reward = 0
+    player_idx, police_idx = si2ai(state)
+    if player_idx == 5:
+        reward += 1
+    if police_idx == player_idx:
+        reward -= 10
     return reward
 
-def next_state(state,action):
-	player_idx,police_idx=si2ai(state);
-	new_player_idx=move(player_idx,action)
-	police_move=POLICE_ACTIONS[int(np.random.uniform(0,N_POLICE_ACTIONS))]
-	new_police_idx=move(police_idx,police_move);
-	return ai2si(new_player_idx,new_police_idx)
 
-#initialize Q
-Q=np.random.uniform(-1,1,size=(N_STATES,N_ROBBER_ACTIONS));
+def next_state(state, action):
+    player_idx, police_idx = si2ai(state)
+    new_player_idx = move(player_idx, action)
+    police_move = POLICE_ACTIONS[int(np.random.uniform(0, N_POLICE_ACTIONS))]
+    new_police_idx = move(police_idx, police_move)
+    return ai2si(new_player_idx, new_police_idx)
 
-print(str(Q));
+# initialize Q
+#Q = np.random.uniform(-1, 1, size=(N_STATES, N_ROBBER_ACTIONS))
+Q = np.ones((N_STATES,N_ROBBER_ACTIONS))
 
+rewards = np.zeros((N_STATES, N_ROBBER_ACTIONS))
+for state in range(0, N_STATES):
+	for a_idx, action in enumerate(ROBBER_ACTIONS):
+		rewards[state, a_idx] = reward(state, action)
+
+initial_state_value=np.zeros(int(n_iterations/ivs_step));
+
+
+state=np.random.randint(0,N_STATES);
+for t in range(0,n_iterations):
+	action=ROBBER_ACTIONS[int(np.random.uniform(0, N_ROBBER_ACTIONS))];
+	new_state=next_state(state,action);
+	Q[state,action]=(1-learning_rate)*Q[state,action]+learning_rate*(reward(state,action)+discount_factor*np.max(Q[new_state,:]));
+	state=new_state;
+	if t%ivs_step==0:
+		print(Q[INITAL_STATE,:])
+		initial_state_value[int(t/ivs_step)]=np.max(Q[INITAL_STATE,:]);
+
+
+
+
+plt.plot(initial_state_value);
+
+plt.show();
